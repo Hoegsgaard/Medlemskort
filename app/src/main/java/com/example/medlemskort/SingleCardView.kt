f43fe6bc9182d4f17302f917dffa31ecf8b210b0
Daniel Greibe <danielgreibe@gmail.com>
2019-10-22 16:06:21 +0200

    ad

---
+++ app/src/main/java/com/example/medlemskort/SingleCardView.kt
@@ -0,0 +1,12 @@
+package com.example.medlemskort
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SingleCardView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_card_view)
+    }
+}
