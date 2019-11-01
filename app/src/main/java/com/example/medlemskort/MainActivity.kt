25099f9f7da88a1d16e08c1d48313b9fe6fc7ca2
Daniel Greibe <danielgreibe@gmail.com>
2019-11-01 08:15:54 +0100

    Converted to One  activity (MainActivity) and rest Fragments. Put all XML from MainActivity into a LoginFragment file.

--- app/src/main/java/com/example/medlemskort/MainActivity.kt
+++ app/src/main/java/com/example/medlemskort/MainActivity.kt
@@ -3,17 +3,14 @@ package com.example.medlemskort
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.medlemskort.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        loginButton.setOnClickListener() {
-            val intent = Intent(this, navigation::class.java)
-            startActivity(intent);
-//Kan du læse dette daniel?
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         }
     }
-}
