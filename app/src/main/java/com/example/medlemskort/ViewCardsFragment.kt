631858d4c42a8ebef7c9504b9511edcc6c0a766c
Daniel Greibe <danielgreibe@gmail.com>
2019-11-11 22:31:57 +0100

    Updated ViewCards Fragment

--- app/src/main/java/com/example/medlemskort/ViewCardsFragment.kt
+++ app/src/main/java/com/example/medlemskort/ViewCardsFragment.kt
@@ -20,15 +20,33 @@ class ViewCardsFragment : Fragment() {
             inflater, R.layout.fragment_view_cards, container, false
         )
 
-        binding.button2.setOnClickListener { view: View ->
+        binding.card1Imageview.setOnClickListener { view: View ->
             Navigation.findNavController(view)
                 .navigate(R.id.action_fragment_view_cards_to_fragment_view_card_barcode)
         }
 
-        binding.button3.setOnClickListener { view: View ->
+        binding.card2Imageview.setOnClickListener { view: View ->
+            Navigation.findNavController(view)
+                .navigate(R.id.action_fragment_view_cards_to_fragment_view_card_barcode)
+        }
+
+        binding.card3Imageview.setOnClickListener { view: View ->
+            Navigation.findNavController(view)
+                .navigate(R.id.action_fragment_view_cards_to_fragment_view_card_barcode)
+        }
+
+        binding.card4Imageview.setOnClickListener { view: View ->
+            Navigation.findNavController(view)
+                .navigate(R.id.action_fragment_view_cards_to_fragment_view_card_barcode)
+        }
+
+        binding.newCardFloatingActionButton.setOnClickListener { view: View ->
             Navigation.findNavController(view)
                 .navigate(R.id.action_fragment_view_cards_to_fragment_create_card_template)
         }
+
+
+
         return binding.root
     }
 
